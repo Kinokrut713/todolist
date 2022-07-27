7354ce6610d6f676e1625280069c23471e81326b
Dmitry Sysoev <rjpzdrf@mail.ru>
2022-07-27 22:38:12 +0300

    v3

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-class Model {
-  constructor() {}
-  }
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+  document.getElementById('clear').addEventListener('click',clearList);
+  document.querySelector('ul').addEventListener('click',deleteOrTick);
 
-class View {
-    constructor() {}
+}
+// submit data function
+function submit(e){
+  e.preventDefault();
+  let taskList;
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+//Добавление задач
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class ="delete">x</span><input type="checkbox"><lable>${task}</lable>`;
+  ul.appendChild(li);
+  document.querySelector('.taskBoard').style.display = 'block';
+}
+
+//очистка листа
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
+//удалить тикет
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
     }
+}
 
-  class Controller {
-    constructor(model, view) {
-this.model = model
-this.view = view
-    }
+//удаление задчи
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+//tick a task
+function tickTask(e) {
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.twxtDecoration = "line-through";
+    task.style.color = "#ff0000";
+  } else {
+    task.style.textDecoration = "none";
+    task.style.color = "#2f4f4f";
   }
-
-  const app = new Controller(new Model(), new View())
+}
